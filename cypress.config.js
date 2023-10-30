b70b04e67e9c138af2807ada0a28d7ed546390de
jaaronbernal <jesus.bernal@99minutos.com>
2023-10-30 17:32:46 -0600

    adding plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,45 @@
 const { defineConfig } = require("cypress");
 
+const createBlunder = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = 
+require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin
+const createEsbuildPlugin = 
+require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin
+
+
+
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+
+    experimentalModifyObstructiveThirdPartyCode: true,
+    // sepupNodeEvents: Sustituye el folder de plugins 
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+      const bulder = createBlunder({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bulder);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+
+
     },
+
+    // ruta donde tenemos los archivos features 
+    //specPattern: "cypress/Warehouse/e2e/features/*.feature",
+    specPattern: "cypress/proyecto/e2e/features/*.feature",
+    // Pagina por default
+    viewportWidth: 1400,
+    viewportHeight: 900,
+    chromeWebSecurity: false,
+    //Tiempo en lo que espara para cargar la pagina
+    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 60000
+
+    
   },
 });
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//upload file
+import 'cypress-file-upload';
+
+import 'cypress-iframe';
